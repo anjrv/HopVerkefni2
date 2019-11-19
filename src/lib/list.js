fc5d6f11d99bb68c3ce137e4c6b95b989fafdbb1
Jaan Jaerving <jaj20@hi.is>
2019-11-19 21:43:56 +0000

    initial js for index page

--- src/lib/list.js
+++ src/lib/list.js
@@ -17,20 +17,52 @@ export default class List {
       });
   }
 
-  // TODO parse information to html elements
-  /* createComponent(data) {
+  createItem(object) {
+    const image = el('div');
+    image.classList.add('lecture__image');
 
-  } */
+    if (object.thumbnail) {
+      const img = el('img');
+      img.setAttribute('src', object.thumbnail);
+      img.setAttribute('alt', '');
+      image.appendChild(img);
+    }
 
-  createLectureList(data) {
-    const dataArray = Object.entries(data);
-    console.log(dataArray);
+    const category = el('span', object.category);
+    category.classList.add('lecture__category');
+
+    const heading = el('h2', object.title);
+    heading.classList.add('lecture__title');
+
+    const textElements = el('div', category, heading);
+    textElements.classList.add('lecture__texts');
+
+    const text = el('div', textElements);
+    text.classList.add('lecture__bottom');
+
+    const item = el('a', image, text);
+    item.classList.add('lecture__button');
+    item.setAttribute('href', `fyrirlestur.html?slug=${object.slug}`);
+
+    return item;
+  }
+
+  // Early variant, need to add code to account for lectures__row
+  createItemList(data) {
+    const myData = Object.keys(data).map((key) => data[key]);
+    const lectures = document.getElementsByClassName('lectures');
+    // Get inner values
+    myData[0].forEach((object) => {
+      const lecturesColumn = el('div', this.createItem(object));
+      lecturesColumn.classList.add('lectures__col');
+      lectures[0].appendChild(lecturesColumn);
+    });
   }
 
   load() {
     empty(this.container);
     this.getData()
-      .then((data) => this.createLectureList(data))
+      .then((data) => this.createItemList(data))
       // TODO:
       // .then((data) => TODO, function to filter lectures )
       // .then((data) => TODO, function to show selected lectures )
