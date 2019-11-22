d0e20b9a42eb578800b09336e9c5d60144088b58
anjrv <55950195+anjrv@users.noreply.github.com>
2019-11-22 08:26:56 +0000

    TODO for completion function

--- src/lib/storage.js
+++ src/lib/storage.js
@@ -3,25 +3,11 @@
  * Skila JSON af localStorage
  */
 export function loadLectures() {
-  const completed = JSON.parse(localStorage.getItem('completed_lecture'));
+  const completed = JSON.parse(localStorage.getItem('completed_lectures'));
   return completed;
 }
 
-/**
- * Uppfæra localStorage þegar einhver "slug" er vistað.
- *
- * Tekur JSON return úr localStorage og bætir við það sem er verið að vista.
- * @param {*} slug slug skilgreining úr lectures.json
- */
-export function saveLecture(slug) {
-  const completed = loadLectures();
-
-  const index = completed.indexOf(slug);
-
-  if (index >= 0) {
-    completed.splice(index, 1);
-  } else {
-    completed.push(slug);
-  }
-  localStorage.setItem('completed_lecture', JSON.stringify(completed));
+export function completeLecture(slug) {
+  // TODO vista núverandi slug í localStorage ef verið er að skrá fyrirlestur sem kláraður
+  // fjarlægja núverandi slug ef verið er að skrá fyrirlestur sem ekki kláraður
 }
